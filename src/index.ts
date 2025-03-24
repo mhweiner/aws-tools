ba51427d304814148c84d09aba24eea8010d765f
Marc H. Weiner <mhweiner234@gmail.com>
2025-03-24 18:57:32 -0400

    feat!: removed unnecessary docker.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import * as dockerMod from './lib/docker';
 import * as ecrMod from './lib/ecr';
 import * as cfnMod from './lib/cfn';
 import * as stsMod from './lib/sts';
@@ -6,6 +5,5 @@ import * as elbMod from './lib/elb';
 
 export const cfn = cfnMod;
 export const ecr = ecrMod;
-export const docker = dockerMod;
 export const sts = stsMod;
 export const elb = elbMod;
